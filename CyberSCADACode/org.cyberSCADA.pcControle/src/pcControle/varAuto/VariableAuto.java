350ec00e905e0a2dc037befa5f597d23db077fce
falcon <falcon@falcon-CR70-2M-CX70-2OC-CX70-2OD>
2016-03-28 22:13:23 +0200

    Revert "La version 1 du projet: version montré lors de la démonstration de la pré-soutenance"
    This reverts commit f007181f421880bd52f29d3347a11e90334db6fe.

--- CyberSCADACode/org.cyberSCADA.pcControle/src/pcControle/varAuto/VariableAuto.java
+++
@@ -1,5 +0,0 @@
-package pcControle.varAuto;
-
-public enum VariableAuto {
-	capteurPresence, actionneurChuteHaut, actionneurChuteBas, moteurBalle, running, remplissage, tournerPlateau
-}
--- CyberSCADACode/org.cyberSCADA.rasFire/src/rasFire/varAuto/VariableAuto.java
+++
@@ -1,5 +0,0 @@
-package rasFire.varAuto;
-
-public enum VariableAuto {
-	capteurPresence, actionneurChuteHaut, actionneurChuteBas, moteurBalle, running, remplissage, tournerPlateau 
-}
